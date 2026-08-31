f7871df73ac210f970c04c154a85d8753c6b825c
wujun <767920412@qq.com>
2026-08-31 10:02:29 +0800

    Initial commit

---
+++ model/my_model.py
@@ -0,0 +1,43 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import os
+#读取配置文件
+load_dotenv()
+"""
+基于单例模式的模型封装
+"""
+class MyModel:
+    #定义私有属性
+    _model = None
+    #定义本地模型私有属性
+    _local_model=None
+    #定义静态函数,在线模型
+    @staticmethod
+    def get_model():
+        #判断_model 是否未空
+        if MyModel._model is None:
+            #创建模型
+            MyModel._model = ChatOpenAI(
+                model= os.getenv("MODEL_LINE_NAME"),
+                api_key=os.getenv("DASHSCOPE_API_KEY"),
+                streaming=True,#开启流式输出
+            )
+        return MyModel._model
+    #本地模型创建
+    @staticmethod
+    def get_local_model():
+        #判断_model 是否未空
+        if MyModel._local_model is None:
+            #创建模型
+            MyModel._local_model = ChatOpenAI(
+                model= os.getenv("MODEL_LOCAL_NAME"),
+                #api_key="ddd",
+                base_url=os.getenv("LOCAL_URL"),
+                streaming=True,#开启流式输出
+            )
+        return MyModel._local_model
+if __name__ =="__main__":
+    model = MyModel.get_local_model()
+    rs = model.invoke("你好")
+    print(rs)
+
